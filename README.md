ba09c959ce0c8b9ca12c05af029e4549213495b4
dafascoder <shirwa256@gmail.com>
2022-11-18 19:05:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Welcome to my portfolio
+
+This is a portfolio of my work. It is a work in progress.
+
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [About Me](#about-me)
+
+## About Me
+
+My name is Shirwa Egal. I am a **Freelance Software Engineer**. I'm using this to consolidate my work and to show off my skills.
+
+- [Project 1]
+- [Project 2]
+
+Built with Astro. Hosted on Netlify.
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE =
+  "Welcome to where I beat up code and make it do what I want it to do.";
+export const SITE_DESCRIPTION =
+  "I am a freelance software engineer who loves to build things. I am currently working on a few projects, but I am always looking for new opportunities. If you have a project you would like to discuss, please contact me.";
